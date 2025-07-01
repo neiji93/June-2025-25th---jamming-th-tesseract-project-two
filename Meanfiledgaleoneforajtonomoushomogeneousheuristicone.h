781bcf293206eae1ba295dd7a3f4d21c41951ca0
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-07-01 17:03:58 +0200

    Create Meanfiledgaleoneforajtonomoushomogeneousheuristicone.h

---
+++ Meanfiledgaleoneforajtonomoushomogeneousheuristicone.h
@@ -0,0 +1,4 @@
+//the source code in the tesseract may contain 
+//experimental maths related and intherited behaviour.
+//jamming with perfect matched particle conductor may help checking
+//maxqda models.
